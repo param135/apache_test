dd0287905061eb42b2a516556c2ff55c16c9ce24
param135 <param135@gmail.com>
2018-05-21 13:54:05 +0000

    changed the template content and managed the version in the metadata

--- recipes/server.rb
+++ recipes/server.rb
@@ -13,11 +13,15 @@ package 'httpd'
 # action :remove
 #end
 
-file '/var/www/html/index.html' do
- content "<h1>Hello, World, We are Coming</h1>
- <h2>ipaddress: #{node['ipaddress']}</h2>
- <h2>hostname: #{node['hostname']}</h2>
-"
+#file '/var/www/html/index.html' do
+ #content "<h1>Hello, World, We are Coming</h1>
+ #<h2>ipaddress: #{node['ipaddress']}</h2>
+ #<h2>hostname: #{node['hostname']}</h2>
+#"
+#end
+
+template '/var/www/html/index.html' do
+ source 'index.html.erb'
 end
 
 service 'httpd' do
